1fb61b0009cf905d47c16aa29b3850c716011fa1
Nicholas Adams <nicholasadams@Owners-MBP.lan>
2024-06-18 14:58:23 -0700

    changing the data

--- test.py
+++ test.py
@@ -26,4 +26,16 @@ df = pd.read_csv('./poke.data.csv')
 # Sorting by 1 field in data 
 #   print(df.sort_values('Name'))
 # Sorting by multiple fields in data
-print(df.sort_values(['Type 1', 'HP'], ascending=[1,0]))
+#   print(df.sort_values(['Type 1', 'HP'], ascending=[1,0]))
+
+
+# Changing data
+#   df['Total'] = df['HP'] + df['Attack'] + df['Defense'] + df['Sp. Atk'] + df['Sp. Def'] + df['Speed']
+#   print(df.head(3))
+# Drop a column
+#df = df.drop(columns=['Total'])
+print(df.head(3))
+# Move column in print out
+cols = list(df.columns.values)
+df = df[cols[0:4] + [cols[-1]] + cols[4:12]]
+print(df.head(3))
