9c0c05ea008c63fcee9b590823fff27798352e89
Nicholas Adams <nicholasadams@Owners-MBP.lan>
2024-06-18 13:28:34 -0700

    reading data

--- test.py
+++ test.py
@@ -1,5 +1,29 @@
 import pandas as pd
 
+# importing the csv file with the data
 df = pd.read_csv('./poke.data.csv')
 
-print(df.head(3))
+# Read headers
+#   print(df.columns)
+
+# Describe the data in dataframe
+#   print(df.describe())
+
+# Read each column
+#   names = df[['Name', 'Type 1', 'HP']][0:3]
+#   print(names)
+
+# Read each row
+#     print(df.iloc)
+
+# Itterate through all rows and get index
+#for index, row in df.iterrows():
+    #print(index, row['Name'])
+
+# Get item by field in dataset
+#    print(df.loc[df['Type 1'] =='Fire'])
+
+# Sorting by 1 field in data 
+#   print(df.sort_values('Name'))
+# Sorting by multiple fields in data
+print(df.sort_values(['Type 1', 'HP'], ascending=[1,0]))
